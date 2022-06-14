2d81ab28d6a8a29abcea1d5812cd221353c6ed80
jandershock <jordan.andershock@gmail.com>
2022-06-14 16:01:59 -0500

    End of day backup

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Models;
+using Microsoft.Data.SqlClient;
+
+namespace TabloidCLI.Repositories
+{
+    public class JournalRepository : DatabaseConnector, IRepository<Journal>
+    {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+        
+        public List<Journal> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Journal Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Journal entry)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"I";
+                }
+            }
+        }
+
+        public void Update(Journal entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
