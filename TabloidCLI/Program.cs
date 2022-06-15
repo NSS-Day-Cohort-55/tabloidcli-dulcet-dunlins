3878ab9a7baa44f2bc679f6ff162f23c1a17b815
karlarn <karlarnichols@gmail.com>
2022-06-15 10:37:05 -0500

    Edited to only show up at the start of the console

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -1,5 +1,5 @@
-﻿using TabloidCLI.UserInterfaceManagers;
-using System;
+﻿using System;
+using TabloidCLI.UserInterfaceManagers;
 
 
 
@@ -12,10 +12,49 @@ namespace TabloidCLI
 
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
+            bool justEnteredConsole = true;
             while (ui != null)
             {
-                Console.WriteLine("Hello! have a pleasant day");
-                Console.WriteLine("--------------------------");
+
+                while (justEnteredConsole)
+                {
+                    Console.WriteLine("Hello! have a pleasant day");
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine(@"Lets put some color in your console.
+1 - Black
+2 - Blue
+3 - Red
+4 - Yellow");
+                    string numForBackground = Console.ReadLine();
+
+                    switch (numForBackground)
+                    {
+                        case "1":
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            break;
+
+                        case "2":
+                            Console.BackgroundColor = ConsoleColor.DarkCyan;
+                            Console.Clear();      
+                            break;
+                        case "3":
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            break;
+                        case "4":
+                            Console.BackgroundColor = ConsoleColor.DarkYellow;
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.WriteLine("Something went wrong, no color was chosen.");
+                            break;
+                    }
+                    justEnteredConsole = false;
+
+                }
+
+
 
                 // Each call to Execute will return the next IUserInterfaceManager we should execute
                 // When it returns null, we should exit the program;
