9108d9d3502727df37efd3445237ebf3212a2940
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-06-15 10:08:11 -0500

    Added Blog repo and manager

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.Repositories
+{
+    internal class BlogRepository : DatabaseConnector,
+        IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base
+            (connectionString) { }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetAll()
+       {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+              using (SqlCommand cmd = conn.CreateCommand())
+               {
+                  cmd.CommandText = @"";
+                }
+                return GetAll();
+           }
+        }
+
+        public void Insert(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Blog
+                                       (Title, Url) 
+                                        VALUES
+                                       (@title, @url)";
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+
+
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+        public void Update(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
