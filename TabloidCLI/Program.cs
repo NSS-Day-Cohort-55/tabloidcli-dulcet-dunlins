075d780afe484c7bcce52f2a0cc778dded43859b
karlarn <karlarnichols@gmail.com>
2022-06-15 11:29:10 -0500

    Removed the meaningless while loop.It will not be missed.

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -12,50 +12,41 @@ namespace TabloidCLI
 
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
-            bool justEnteredConsole = true;
-            while (ui != null)
-            {
-
-                while (justEnteredConsole)
-                {
-                    Console.WriteLine("Hello! have a pleasant day");
-                    Console.WriteLine("--------------------------");
-                    Console.WriteLine(@"Lets put some color in your console.
+            Console.WriteLine("Hello! have a pleasant day");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(@"Lets put some color in your console.
 1 - Gray
 2 - Blue
 3 - Red
 4 - Yellow");
-                    string numForBackground = Console.ReadLine();
+            string numForBackground = Console.ReadLine();
 
-                    switch (numForBackground)
-                    {
-                        case "1":
-                            Console.BackgroundColor = ConsoleColor.DarkGray;
-                            Console.Clear();
-                            break;
+            switch (numForBackground)
+            {
+                case "1":
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.Clear();
+                    break;
 
-                        case "2":
-                            Console.BackgroundColor = ConsoleColor.DarkCyan;
-                            Console.Clear();      
-                            break;
-                        case "3":
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.Clear();
-                            break;
-                        case "4":
-                            Console.BackgroundColor = ConsoleColor.DarkYellow;
-                            Console.Clear();
-                            break;
-                        default:
-                            Console.WriteLine("Something went wrong, no color was chosen.");
-                            break;
-                    }
-                    justEnteredConsole = false;
+                case "2":
+                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    Console.Clear();
+                    break;
+                case "3":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    break;
+                case "4":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.Clear();
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong, no color was chosen.");
+                    break;
+            }
 
-                }
-
-
-
+            while (ui != null)
+            { 
                 // Each call to Execute will return the next IUserInterfaceManager we should execute
                 // When it returns null, we should exit the program;
                 ui = ui.Execute();
