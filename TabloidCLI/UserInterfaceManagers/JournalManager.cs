acbc3685a524df5dc7d24905226828c65c6e1513
jandershock <99635699+jandershock@users.noreply.github.com>
2022-06-15 10:07:23 -0500

    Journal manager insert method (#49)
    * End of day backup
    
    * Added unsaved files to end of day backup
    
    * Finished add journal entry

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Repositories;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) Add Journal Entry");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.WriteLine("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Add();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        public void Add()
+        {
+            Journal journalObject = new Journal();
+            Console.Write("Please enter journal title: ");
+            journalObject.Title = Console.ReadLine();
+
+            Console.Write("Please enter journal content: ");
+            journalObject.Content = Console.ReadLine();
+
+            journalObject.CreateDateTime = DateTime.Now;
+
+            _journalRepository.Insert(journalObject);
+            
+        }
+
+    }
+}
